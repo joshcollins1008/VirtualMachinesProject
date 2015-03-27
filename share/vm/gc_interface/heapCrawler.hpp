05d6a2dfb06a7db0778db7b6af808d202a126c6e
Josh <joshcollins1008@gmail.com>
2015-03-27 15:16:16 -0500

    Fixed some mistakes
    Fixed mistakes

--- share/vm/gc_interface/heapCrawler.hpp
+++ share/vm/gc_interface/heapCrawler.hpp
@@ -1,3 +1,7 @@
+#ifndef SHARE_VM_GC_INTERFACE_HEAPCRAWLER_HPP
+#define SHARE_VM_GC_INTERFACE_HEAPCRAWLER_HPP
+
+
 #include "memory/iterator.hpp"
 
 class OopCrawlerClosure : public OopClosure {
@@ -5,3 +9,5 @@ public:
   void do_oop( oop* o );
   void do_oop( narrowOop* o );
 };
+
+#endif
--- share/vm/project/heapCrawler.cpp
+++
@@ -1,1 +0,0 @@
-#include "project/heapCrawler.hpp"
